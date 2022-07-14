1f6efcb8779ecbe0d28ac5a9e028ee673c6f1006
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-07-13 20:19:42 -0500

    express server set

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req,res)=>{
+    res.send('task manager app test')
+})
+
+
+const port = 6969;
+
+app.listen(port, console.log('listening!!'))
