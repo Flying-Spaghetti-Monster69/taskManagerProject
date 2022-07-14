a3557f7955f41c6194e5b50af666eea4baabf885
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-07-13 20:50:02 -0500

    all routes init

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,23 @@
 const getAllTasks = (req,res)=>{
     res.send('sent request of getting tasks');
 }
+const getTasks = (req,res)=>{
+    res.send('sent request of getting task');
+}
+const changeTasks = (req,res)=>{
+    res.send('sent request of updating tasks');
+}
+const deleteTasks = (req,res)=>{
+    res.send('sent request of deleting tasks');
+}
+const createTasks = (req,res)=>{
+    res.send('sent request of creating tasks');
+}
 
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    getTasks,
+    changeTasks,
+    deleteTasks,
+    createTasks
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {getAllTasks} = require('../controllers/tasks');
+const {
+    getAllTasks,
+    getTasks,
+    createTasks,
+    deleteTasks,
+    changeTasks
+} = require('../controllers/tasks');
 
-router.route('/').get(getAllTasks);
+router.route('/')
+   .get(getAllTasks)
+   .post(createTasks);
+
+router.route('/:id')
+   .get(getTasks)
+   .patch(changeTasks)
+   .delete(deleteTasks);
 
 module.exports = router;
