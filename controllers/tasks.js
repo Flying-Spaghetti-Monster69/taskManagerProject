a667e483b27986089e13842469de95acc3cf387e
Flying-Spaghetti-Monster69 <felipegarzon908@gmail.com>
2022-07-15 11:40:25 -0500

    schema parameters updated and get && post

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,12 @@
-const getAllTasks = (req,res)=>{
-    res.send('sent request of getting tasks');
+const Task = require('../models/Task')
+
+const getAllTasks = async (req,res)=>{
+    try{
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } catch(error){
+        res.status(500).json({msg: error})
+    }
 }
 const getTasks = (req,res)=>{
     res.send(req.params);
@@ -10,8 +17,13 @@ const changeTasks = (req,res)=>{
 const deleteTasks = (req,res)=>{
     res.send('sent request of deleting tasks');
 }
-const createTasks = (req,res)=>{
-    res.send(req.body);
+const createTasks = async (req,res)=>{
+    try{
+        const task = await Task.create(req.body);
+        res.status(201).json({task});
+    } catch(error){
+        res.status(500).json({msg: error})
+    }
 }
 
 module.exports = {
